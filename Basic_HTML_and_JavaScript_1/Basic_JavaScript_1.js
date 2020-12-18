4135e3ff0fb83a6c0dd568c3403f871d568c8b38
srnovak13 <srnovak@gmail.com>
2020-12-17 16:31:50 -0800

    Basic HTML and CSS Assignments

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello World! I'm an alert box!");
+
+function My_First_Function() {
+		var string="Kiss me, I'm Irish";
+		var result=String.fontcolor("green");
+		document.getElementById("Irish").innerHTML=
+		result;
+		}
