6363c3f8b9f97cf6ce9c7c0a272abcafc66e3f83
srnovak13 <srnovak@gmail.com>
2021-01-18 14:59:56 -0800

    Add JS Lightbox

---
+++ One-Page Website/js/one_page_website.js
@@ -0,0 +1,40 @@
+// OPEN THE MODAL
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // CLOSE THE MODAL
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // NEXT/PREVIOUS CONTROLS
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // THUMBNAIL IMAGE CONTROLS
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
